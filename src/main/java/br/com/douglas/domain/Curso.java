7257618a6dbde46db41a439b5d9bea82780b370a
Douglas <douglasmsrodrigues@hotmail.com>
2026-05-11 11:30:07 -0300

    Update

---
+++ src/main/java/br/com/douglas/domain/Curso.java
@@ -0,0 +1,58 @@
+package br.com.douglas.domain;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="TB_CURSO")
+public class Curso {
+
+    @Id
+    @GeneratedValue(strategy=GenerationType.SEQUENCE, generator="cliente_seq")
+	@SequenceGenerator(name="cliente_seq", sequenceName="sq_cliente", initialValue = 1, allocationSize = 1)
+    private Long id;
+
+    private String codigo;
+
+    private String nome;
+
+    private String descricao;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+
+}
